5c701452e632891f9cc7058080a28b0169e345d4
Pär Dahlman <par.dahlman@fellowdeveloper.se>
2020-04-12 12:29:26 +0200

    Make nvm calls in expression

--- setup.ps1
+++ setup.ps1
@@ -35,8 +35,8 @@ ScoopInstallOrUpdate("z")
 ScoopInstallOrUpdate("dotnet-sdk")
 ScoopInstallOrUpdate("posh-git")
 ScoopInstallOrUpdate("nvm")
-nvm install 12.16.2
-nvm use 12.16.2
+Invoke-Expression "nvm install 12.16.2"
+Invoke-Expression "nvm use 12.16.2"
 ScoopInstallOrUpdate("yarn")
 ScoopInstallOrUpdate("busybox")
 ScoopInstallOrUpdate("vim")

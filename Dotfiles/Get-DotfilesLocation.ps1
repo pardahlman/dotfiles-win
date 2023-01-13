d493f81fd8960d047a78aa3779eca059332825ae
Pär Dahlman <par.dahlman@fellowdeveloper.se>
2023-01-13 06:33:37 +0100

    Support junctions in Get-DotfilesLocation

--- Dotfiles/Get-DotfilesLocation.ps1
+++ Dotfiles/Get-DotfilesLocation.ps1
@@ -1,3 +1,10 @@
 function Get-DotfilesLocation {
-    return $PSScriptRoot | Split-Path
+    # Assume this file is located in root of module
+    $ModulePath = Get-Item ($PSScriptRoot)
+
+    if($ModulePath.LinkType -eq 'Junction')
+    {
+        $ModulePath = Select-Object -InputObject $ModulePath -ExpandProperty Target | Get-Item
+    }
+    return $ModulePath.FullName | Split-Path
 }
